970c14bac60939afbee4fc7b5504d43c3c0a77f7
davidgoitia <david@claviux.com>
2018-02-25 21:05:29 +0100

    change running test

--- entry.sh
+++ entry.sh
@@ -9,12 +9,7 @@ $CMD start
 date
 echo Alfresco is ready for action
 
-while : ; do
-  $CMD status >/dev/null 2>&1
-  _rc=$?
-  if (($_rc)) ; then
-    exit $_rc
-  else
-    sleep 5
-  fi
+while [[ "$($CMD status | grep -i "not running")" == "" ]] ; do
+    sleep 10
 done
+exit 1
